81058b3f2bc2e1c884a4f5094544ed9c4744342c
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-01-03 17:41:12 +0100

    fix: Corrected test logs

--- run-tests.sh
+++ run-tests.sh
@@ -21,20 +21,20 @@ fi
 # Run Graphics Tests
 #
 
-echo "2020 LTS URP EditMode"
+echo "2020 LTS URP Graphics"
 time $UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-urp -testResults "$PWD/test-results/glTFast-graphics-tests-urp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
 
-echo "2020 LTS HDRP EditMode"
+echo "2020 LTS HDRP Graphics"
 time $UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-hdrp -testResults "$PWD/test-results/glTFast-graphics-tests-hdrp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
 
-echo "2020 LTS BiRP EditMode"
+echo "2020 LTS BiRP Graphics"
 time $UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-birp -testResults "$PWD/test-results/glTFast-graphics-tests-birp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
 
-echo "2021 LTS URP EditMode"
+echo "2021 LTS URP Graphics"
 time $UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-urp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-urp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
 
-echo "2021 LTS HDRP EditMode"
+echo "2021 LTS HDRP Graphics"
 time $UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-hdrp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-hdrp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
 
-echo "2021 LTS BiRP EditMode"
+echo "2021 LTS BiRP Graphics"
 time $UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-birp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-birp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
