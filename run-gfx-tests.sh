ea2e822cb5a5d7cae8b5dec902de4b0018420485
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-03-08 18:08:26 +0100

    feat: Switching to runtime graphics tests (not editor only anymore)

--- run-gfx-tests.sh
+++ run-gfx-tests.sh
@@ -8,23 +8,25 @@ source unity.sh
 # Run Graphics Tests
 #
 
+TEST_PLATFORM=StandaloneOSX
+
 echo "2020 LTS URP Graphics"
-$UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-urp -testResults "$PWD/test-results/glTFast-graphics-tests-urp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-urp -testResults "$PWD/test-results/glTFast-graphics-tests-urp.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
 
 echo "2020 LTS HDRP Graphics"
-$UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-hdrp -testResults "$PWD/test-results/glTFast-graphics-tests-hdrp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-hdrp -testResults "$PWD/test-results/glTFast-graphics-tests-hdrp.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
 
 echo "2020 LTS BiRP Graphics"
-$UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-birp -testResults "$PWD/test-results/glTFast-graphics-tests-birp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2020_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-birp -testResults "$PWD/test-results/glTFast-graphics-tests-birp.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
 
 echo "2021 LTS URP Graphics"
-$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-urp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-urp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-urp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-urp.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
 
 echo "2021 LTS HDRP Graphics"
-$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-hdrp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-hdrp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-hdrp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-hdrp.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
 
 echo "2021 LTS BiRP Graphics"
-$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-birp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-birp-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-birp -testResults "$PWD/test-results/glTFast-graphics-tests-2021-birp.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
 
 echo "2021 LTS BiRP Shader Graph Graphics"
-$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-birp-sg -testResults "$PWD/test-results/glTFast-graphics-tests-2021-birp-sg-editor.xml" -testPlatform PlayMode -testCategory "Graphics"
+$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-graphics-tests-2021-birp-sg -testResults "$PWD/test-results/glTFast-graphics-tests-2021-birp-sg.xml" -testPlatform "$TEST_PLATFORM" -testCategory "Graphics"
