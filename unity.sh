0d0710b74b80b6bc82cf281b406fe9a8d8e2aaff
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-03-16 17:40:15 +0100

    chore: various updates

--- unity.sh
+++ unity.sh
@@ -4,7 +4,7 @@ PWD=$(pwd)
 
 UNITY_2019=/Applications/Unity/Hub/Editor/2019.4.36f1
 UNITY_2020=/Applications/Unity/Hub/Editor/2020.3.30f1
-UNITY_2021=/Applications/Unity/Hub/Editor/2021.2.14f1
+UNITY_2021=/Applications/Unity/Hub/Editor/2021.2.15f1
 
 UNITY_2019_EXE="$UNITY_2019/Unity.app/Contents/MacOS/Unity"
 UNITY_2020_EXE="$UNITY_2020/Unity.app/Contents/MacOS/Unity"
