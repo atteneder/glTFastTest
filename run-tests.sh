7daed69178b472f3cc07e4cda844ce34ca96a9b3
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-04-14 17:05:05 +0200

    fix: Moved commented UTR code to top. Added 2019 HDRP

--- run-tests.sh
+++ run-tests.sh
@@ -12,17 +12,25 @@ PLAYMODE_PLATFORM=StandaloneOSX
 export GLTF_TEST_ASSET_DIR=$(pwd)/assets
 
 # PROJECT="./projects/glTFast-tests-2019"
+# echo "2019 LTS BiRP EditMode"
+# UnifiedTestRunner --suite=editor  --testproject="$PROJECT" --editor-location="$UNITY_2019"
+# echo "2019 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
+# UnifiedTestRunner --suite=playmode --testlist="testlist.txt" --testproject="$PROJECT" --editor-location="$UNITY_2019" --platform="$PLAYMODE_PLATFORM" --a="$PWD/test-results/glTFast-tests-2019-runtime"
+
 echo "2019 LTS BiRP EditMode"
 $UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019 -testResults "$PWD/test-results/glTFast-tests-2019-birp-editor.xml" -testPlatform EditMode
-#UnifiedTestRunner --suite=editor  --testproject="$PROJECT" --editor-location="$UNITY_2019"
 echo "2019 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
 $UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019 -testResults "$PWD/test-results/glTFast-tests-2019-birp-runtime.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "!Performance"
-#UnifiedTestRunner --suite=playmode --testlist="testlist.txt" --testproject="$PROJECT" --editor-location="$UNITY_2019" --platform="$PLAYMODE_PLATFORM" --a="$PWD/test-results/glTFast-tests-2019-runtime"
 
 echo "2019 LTS URP EditMode"
 $UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019-urp -testResults "$PWD/test-results/glTFast-tests-2019-urp-editor.xml" -testPlatform EditMode
 echo "2019 LTS URP PlayMode $PLAYMODE_PLATFORM"
 $UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019-urp -testResults "$PWD/test-results/glTFast-tests-2019-urp-runtime.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "!Performance"
+
+echo "2019 LTS HDRP EditMode"
+$UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019-hdrp -testResults "$PWD/test-results/glTFast-tests-2019-hdrp-editor.xml" -testPlatform EditMode
+echo "2019 LTS HDRP PlayMode $PLAYMODE_PLATFORM"
+$UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019-hdrp -testResults "$PWD/test-results/glTFast-tests-2019-hdrp-runtime.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "!Performance"
 
 echo "2021 BiRP EditMode"
 $UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2021 -testResults "$PWD/test-results/glTFast-tests-2021-birp-editor.xml" -testPlatform EditMode
