2e577dadf450daf4553f561e6e74d0269b122c28
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-04-23 00:21:42 +0200

    fix: Consistent file names

--- run-tests.sh
+++ run-tests.sh
@@ -39,9 +39,9 @@ echo "2021 URP PlayMode $PLAYMODE_PLATFORM"
 $UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2021-urp -testResults "$PWD/test-results/glTFast-tests-2021-urp-runtime.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "!Performance"
 
 echo "2022 BiRP EditMode"
-$UNITY_2022_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2022 -testResults "$PWD/test-results/glTFast-tests-2022-editor.xml" -testPlatform EditMode
+$UNITY_2022_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2022 -testResults "$PWD/test-results/glTFast-tests-2022-birp-editor.xml" -testPlatform EditMode
 echo "2022 BiRP PlayMode $PLAYMODE_PLATFORM"
-$UNITY_2022_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2022 -testResults "$PWD/test-results/glTFast-tests-2022-runtime.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "!Performance"
+$UNITY_2022_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2022 -testResults "$PWD/test-results/glTFast-tests-2022-birp-runtime.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "!Performance"
 
 echo "2022 HDRP EditMode"
 $UNITY_2022_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2022-hdrp -testResults "$PWD/test-results/glTFast-tests-2022-hdrp-editor.xml" -testPlatform EditMode
