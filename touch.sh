33e86ce8ea9ee5f783b80691d9d78c190aaa83e2
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-05-31 11:26:48 +0200

    feat: Added 2020 projects fix: Removed duplicated project calls

--- touch.sh
+++ touch.sh
@@ -4,54 +4,43 @@ set -e
 
 source unity.sh
 
-echo "2019 LTS BiRP EditMode"
-$UNITY_2019_EXE -batchmode -projectPath ./projects/glTFast-tests-2019 -quit
-echo "2019 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
+echo "2019 BiRP"
 $UNITY_2019_EXE -batchmode -projectPath ./projects/glTFast-tests-2019 -quit
 
-echo "2019 LTS URP EditMode"
-$UNITY_2019_EXE -batchmode -projectPath ./projects/glTFast-tests-2019-urp -quit
-echo "2019 LTS URP PlayMode $PLAYMODE_PLATFORM"
+echo "2019 URP"
 $UNITY_2019_EXE -batchmode -projectPath ./projects/glTFast-tests-2019-urp -quit
 
-echo "2019 LTS HDRP EditMode"
-$UNITY_2019_EXE -batchmode -projectPath ./projects/glTFast-tests-2019-hdrp -quit
-echo "2019 LTS HDRP PlayMode $PLAYMODE_PLATFORM"
+echo "2019 HDRP"
 $UNITY_2019_EXE -batchmode -projectPath ./projects/glTFast-tests-2019-hdrp -quit
 
-echo "2021 BiRP EditMode"
+echo "2020 BiRP"
+$UNITY_2020_EXE -batchmode -projectPath ./projects/glTFast-tests-2020 -quit
+
+echo "2020 URP"
+$UNITY_2020_EXE -batchmode -projectPath ./projects/glTFast-tests-2020-urp -quit
+
+echo "2020 HDRP"
+$UNITY_2020_EXE -batchmode -projectPath ./projects/glTFast-tests-2020-hdrp -quit
+
+echo "2021 BiRP"
 $UNITY_2021_EXE -batchmode -projectPath ./projects/glTFast-tests-2021 -quit
-echo "2021 BiRP PlayMode $PLAYMODE_PLATFORM"
-$UNITY_2021_EXE -batchmode -projectPath ./projects/glTFast-tests-2021 -quit
 
-echo "2021 HDRP EditMode"
-$UNITY_2021_EXE -batchmode -projectPath ./projects/glTFast-tests-2021-hdrp -quit
-echo "2021 HDRP PlayMode $PLAYMODE_PLATFORM"
+echo "2021 HDRP"
 $UNITY_2021_EXE -batchmode -projectPath ./projects/glTFast-tests-2021-hdrp -quit
 
-echo "2021 URP EditMode"
-$UNITY_2021_EXE -batchmode -projectPath ./projects/glTFast-tests-2021-urp -quit
-echo "2021 URP PlayMode $PLAYMODE_PLATFORM"
+echo "2021 URP"
 $UNITY_2021_EXE -batchmode -projectPath ./projects/glTFast-tests-2021-urp -quit
 
-echo "2022 BiRP EditMode"
-$UNITY_2022_EXE -batchmode -projectPath ./projects/glTFast-tests-2022 -quit
-echo "2022 BiRP PlayMode $PLAYMODE_PLATFORM"
+echo "2022 BiRP"
 $UNITY_2022_EXE -batchmode -projectPath ./projects/glTFast-tests-2022 -quit
 
-echo "2022 HDRP EditMode"
-$UNITY_2022_EXE -batchmode -projectPath ./projects/glTFast-tests-2022-hdrp -quit
-echo "2022 HDRP PlayMode $PLAYMODE_PLATFORM"
+echo "2022 HDRP"
 $UNITY_2022_EXE -batchmode -projectPath ./projects/glTFast-tests-2022-hdrp -quit
 
-echo "2022 URP EditMode"
-$UNITY_2022_EXE -batchmode -projectPath ./projects/glTFast-tests-2022-urp -quit
-echo "2022 URP PlayMode $PLAYMODE_PLATFORM"
+echo "2022 URP"
 $UNITY_2022_EXE -batchmode -projectPath ./projects/glTFast-tests-2022-urp -quit
 
-echo "DOTS EditMode"
-$UNITY_2020_EXE -batchmode -projectPath ./projects/glTFast-tests-dots -quit
-echo "DOTS PlayMode $PLAYMODE_PLATFORM"
+echo "DOTS"
 $UNITY_2020_EXE -batchmode -projectPath ./projects/glTFast-tests-dots -quit
 
 echo "glTFast-tests-min-feature macOS Build"
