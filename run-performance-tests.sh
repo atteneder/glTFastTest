d65f6600b38a3aefdceb39765019ce68035bc4d5
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-06-07 11:16:46 +0200

    feat: Performance tests script

---
+++ run-performance-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+source unity.sh
+
+#
+# Run Performance Tests
+#
+
+export GLTF_TEST_ASSET_DIR=$(pwd)/assets
+
+echo "2019 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
+$UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019 -testResults "$PWD/test-results/glTFast-tests-2019-birp-performance.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "Performance"
