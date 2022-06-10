bbfde6be37776a5e89c83f56b6bdf341595e4486
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-06-10 14:54:26 +0200

    feat: Using 2021 for performance tests from now on

--- run-performance-tests.sh
+++ run-performance-tests.sh
@@ -10,5 +10,8 @@ source unity.sh
 
 export GLTF_TEST_ASSET_DIR=$(pwd)/assets
 
-echo "2019 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
-$UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019 -testResults "$PWD/test-results/glTFast-tests-2019-birp-performance.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "Performance"
+# echo "2019 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
+# $UNITY_2019_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2019 -testResults "$PWD/test-results/glTFast-tests-2019-birp-performance.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "Performance"
+
+echo "2021 LTS BiRP PlayMode $PLAYMODE_PLATFORM"
+$UNITY_2021_EXE -runTests -batchmode -projectPath ./projects/glTFast-tests-2021 -testResults "$PWD/test-results/glTFast-tests-2021-birp-performance.xml" -testPlatform "$PLAYMODE_PLATFORM" -testCategory "Performance"
